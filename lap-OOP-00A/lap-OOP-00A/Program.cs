37e70c77b460375cc720792fa79a96bbeeea2f7d
MDM007MDM <chadaporn.pin@kkumail.com>
2022-11-28 15:39:34 +0700

    lap OOP 00A

---
+++ lap-OOP-00A/lap-OOP-00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// print string to commanline
+Console.WriteLine(" Chadaporn Pinchsai ");
+// ID
+Console.WriteLine(" 653450281-9 ");
+// Email
+Console.WriteLine(" chadaporn Pinichsai ");
